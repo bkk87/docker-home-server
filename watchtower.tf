50b0c22bfe3e422c43fe55d488aee0b8c08f2cc8
bkk87 <github@koch-kemper.de>
2021-04-10 17:18:28 +0200

    watchtower metrics are not really useful

--- watchtower.tf
+++ watchtower.tf
@@ -6,22 +6,14 @@ resource "docker_image" "watchtower" {
   keep_locally = true
 }
 
-resource "random_password" "watchtower_token" {
-  length  = 20
-  special = false
-}
-output "watchtower_token" {
-  value = random_password.watchtower_token.result
-}
 resource "docker_container" "watchtower" {
-  name  = "watchtower"
-  image = docker_image.watchtower.name
+  name   = "watchtower"
+  image  = docker_image.watchtower.name
+  memory = var.watchtower_container_memory_limit
   env = [
     "WATCHTOWER_CLEANUP=true",
     "WATCHTOWER_POLL_INTERVAL=86400",
-    "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-    "WATCHTOWER_HTTP_API_TOKEN=${random_password.watchtower_token.result}",
-    "WATCHTOWER_HTTP_API_METRICS=true"
+    "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
   ]
   restart = "unless-stopped"
   start   = true
