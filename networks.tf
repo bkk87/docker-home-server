d479c46ff5aa2556707ae565429597e4e16d3360
bkk87 <github@koch-kemper.de>
2021-06-11 17:35:27 +0200

    dont allow access on host systems

--- networks.tf
+++ networks.tf
@@ -15,14 +15,6 @@ resource "docker_network" "public_network" {
   name            = "public_network"
   check_duplicate = true
   driver          = "bridge"
-  # Manually run: iptables -I DOCKER-USER -m conntrack --ctstate NEW -s 172.19.0.0/16 -m addrtype --dst-type LOCAL -j DROP
-  # Blocks
-  #     Access to host running docker daemon
-  # Does not block
-  #     Container to container traffic
-  #     Local LAN
-  #     Internet
-  #     Access originating from other docker networks
   ipam_config {
     subnet   = "172.19.0.0/16"
     ip_range = "172.19.0.0/24"
@@ -30,7 +22,7 @@ resource "docker_network" "public_network" {
   }
   options = {
     "com.docker.network.bridge.name"                 = "docker2",
-    "com.docker.network.bridge.enable_ip_masquerade" = "true",
+    "com.docker.network.bridge.enable_ip_masquerade" = "false",
     "com.docker.network.bridge.enable_icc"           = "true",
     "com.docker.network.bridge.host_binding_ipv4"    = "0.0.0.0"
     "com.docker.network.driver.mtu"                  = "1500"
